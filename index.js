a13d924739f8bc090f075883b4c931ac94e0e115
0xflotus <0xflotus@gmail.com>
2019-02-13 00:56:52 +0100

    v0.0.14 cli support via commander

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
+var program = require("commander");
 var { Resolver } = require("dns");
-const args = process.argv.slice(2);
-if (args.length === 0) {
-  console.log(
-    "Usage: dinos [<host>]\n\nFor IPv6: IP=6 dinos [<host>]\n\nExample: dinos localhost"
-  );
-  process.exit(-1);
+
+program
+  .version(require("./package.json").version)
+  .usage("dinos <host>")
+  .option("-m, --max <number>", "Limit output")
+  .option("-6, --IPv6", "Use IPv6")
+  .parse(process.argv);
+
+if (program.args.length === 0) {
+  program.outputHelp();
+  process.exit();
 }
 
 const { servers } = require("./servers.json");
 let count = 0;
 for (const ip of servers) {
   count++;
-  if (count > process.env.MAX) {
+  if (count > program.max) {
     break;
   }
   const resolver = new Resolver();
   resolver.setServers([ip]);
-  args.forEach(host => {
-    resolver[`resolve${process.env.IP === "6" ? 6 : 4}`](host, function(
-      err,
-      addresses
-    ) {
+  program.args.forEach(host => {
+    resolver[`resolve${program.IPv6 ? 6 : 4}`](host, function(err, addresses) {
       if (err) {
         console.log("An error occured with server %s for %s", ip, host);
       } else {
@@ -28,7 +31,7 @@ for (const ip of servers) {
           console.log(
             "%s resolves %s",
             ip,
-            args.length > 1 ? `${addr} for ${host}` : addr
+            program.args.length > 1 ? `${addr} for ${host}` : addr
           )
         );
       }
