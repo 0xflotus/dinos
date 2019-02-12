491a5959d7a5609442b6deae8842df55e2e813f2
0xflotus <0xflotus@gmail.com>
2019-02-12 13:50:38 +0100

    IPv6 support

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const args = process.argv.slice(2);
 if (!args.length) {
   console.log(`
         Usage: dinos [<host>]
+
+        For IPv6: IP=6 dinos [<host>]
+
+        Example: dinos localhost
   `);
 }
 
@@ -16,15 +20,21 @@ if (!args.length) {
   resolver.setServers([ip]);
 
   args.forEach(host => {
-    resolver.resolve4(host, (err, addresses) => {
-      if (err) throw err;
-      addresses.forEach(addr =>
-        console.log(
-          ip,
-          "resolves",
-          args.length > 1 ? `${addr} for ${host}` : addr
-        )
-      );
-    });
+    resolver[`resolve${process.env["IP"] === "6" ? 6 : 4}`](
+      host,
+      (err, addresses) => {
+        if (err) {
+          console.log(`An error occured with server ${ip} for ${host}`);
+        } else {
+          addresses.forEach(addr =>
+            console.log(
+              ip,
+              "resolves",
+              args.length > 1 ? `${addr} for ${host}` : addr
+            )
+          );
+        }
+      }
+    );
   });
 });
