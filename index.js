d215ef17179712a6f9a4d1e04a2b2ec54fa75272
0xflotus <0xflotus@gmail.com>
2019-02-12 19:58:19 +0100

    v0.0.12

--- index.js
+++ index.js
@@ -28,7 +28,6 @@
     const resolver = new Resolver();
     const ip = split(num["repeat"](~-0x2 << 2))["join"](".");
     resolver["setServers"]([ip]);
-
     args["forEach"](_host => {
       resolver[
         `resolve${
