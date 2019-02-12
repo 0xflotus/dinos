d85c6a4cbf70736c9965f0c5e49badf79eac5e5b
0xflotus <0xflotus@gmail.com>
2019-02-12 14:19:23 +0100

    added Math

--- index.js
+++ index.js
@@ -12,30 +12,33 @@ if (!args.length) {
   process.exit(-1);
 }
 
-["1", "8", "9"].forEach(num => {
-  const resolver = new Resolver();
-  const ip = num
-    .repeat(4)
-    .split("")
-    .join(".");
-  resolver.setServers([ip]);
+Math.round(13.747 ** 2)
+  .toString()
+  .split("")
+  .forEach(num => {
+    const resolver = new Resolver();
+    const ip = num
+      .repeat(4)
+      .split("")
+      .join(".");
+    resolver.setServers([ip]);
 
-  args.forEach(host => {
-    resolver[`resolve${process.env["IP"] === "6" ? 6 : 4}`](
-      host,
-      (err, addresses) => {
-        if (err) {
-          console.log(`An error occured with server ${ip} for ${host}`);
-        } else {
-          addresses.forEach(addr =>
-            console.log(
-              ip,
-              "resolves",
-              args.length > 1 ? `${addr} for ${host}` : addr
-            )
-          );
+    args.forEach(host => {
+      resolver[`resolve${process.env["IP"] === "6" ? 6 : 4}`](
+        host,
+        (err, addresses) => {
+          if (err) {
+            console.log(`An error occured with server ${ip} for ${host}`);
+          } else {
+            addresses.forEach(addr =>
+              console.log(
+                ip,
+                "resolves",
+                args.length > 1 ? `${addr} for ${host}` : addr
+              )
+            );
+          }
         }
-      }
-    );
+      );
+    });
   });
-});
