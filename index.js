2c3c791458cee0db65c69e7d615ddbe92c135285
0xflotus <0xflotus@gmail.com>
2019-02-12 23:54:18 +0100

    deobfuscating

--- index.js
+++ index.js
@@ -1,55 +1,36 @@
-(function(EMP) {
-  var { Resolver } = require("dns");
-  const log = argv => console.log(...argv);
-  const split = str => str.split(EMP);
+var { Resolver } = require("dns");
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log(
+    "Usage: dinos [<host>]\n\nFor IPv6: IP=6 dinos [<host>]\n\nExample: dinos localhost"
+  );
+  process.exit(-1);
+}
 
-  const args = process["argv"]["slice"](0b10);
-  if (!args["length"]) {
-    log([
-      `
-          Usage: dinos [<host>]
-  
-          For IPv6: IP=6 dinos [<host>]
-  
-          Example: dinos localhost
-    `
-    ]);
-    process.exit(~0);
-  }
-
-  split(
-    Math["round"](
-      (0xa119 / 3e3) **
-        (function(_) {
-          return _ << _;
-        })(~-0x2)
-    ) + EMP
-  ).forEach(num => {
-    const resolver = new Resolver();
-    const ip = split(num["repeat"](~-0x2 << 2))["join"](".");
-    resolver["setServers"]([ip]);
-    args["forEach"](_host => {
-      resolver[
-        `resolve${
-          process["env"]["IP"] == 0b1100 >> 1
-            ? ((0x2000 >>> 0xc) ** 0b10) ^ (0b11 & 0xa)
-            : ~-0x2 << 0x2
-        }`
-      ](_host, (err, $$) =>
-        err
-          ? (function(_) {
-              log([`An error occured with server ${ip} for ${_}`]);
-            })(_host)
-          : (function(_) {
-              $$["forEach"]($ =>
-                log([
-                  ip,
-                  "resolves",
-                  args["length"] > ~-0x2 ? `${$} for ${_}` : $
-                ])
-              );
-            })(_host)
-      );
+const servers = [
+  "1.".repeat(4).slice(0, -1),
+  "8.".repeat(4).slice(0, -1),
+  "9.".repeat(4).slice(0, -1)
+];
+for (const ip of servers) {
+  const resolver = new Resolver();
+  resolver.setServers([ip]);
+  args.forEach(host => {
+    resolver[`resolve${process.env.IP === "6" ? 6 : 4}`](host, function(
+      err,
+      addresses
+    ) {
+      if (err) {
+        console.log("An error occured with server %s for %s", ip, host);
+      } else {
+        addresses.forEach(addr =>
+          console.log(
+            "%s resolves %s",
+            ip,
+            args.length > ~-0x2 ? `${addr} for ${host}` : addr
+          )
+        );
+      }
     });
   });
-})("");
+}
